299146f32cd0e01ec88524df7de79002113eed15
Kurt.Quejada <kurtquejada@gmail.com>
2022-11-20 18:52:21 +0800

    Automated testing for BMI calculator

---
+++ Unit Test/BMI_calculator.js
@@ -0,0 +1,13 @@
+let kg =0
+let m= 0
+
+const myFunction = (kg,m) => {
+
+        m = m*m;
+       let value = kg/m;
+      console.log(value.toFixed(2));
+    
+}
+myFunction(75,1.74)
+
+module.exports = myFunction
---
+++ Unit Test/BMI_calculator.test.js
@@ -0,0 +1,4 @@
+test("Check if statement is true", ()=>{
+var status = true;
+expect(status).toBe(true);
+});
