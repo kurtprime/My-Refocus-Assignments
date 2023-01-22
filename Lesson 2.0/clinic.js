b0adce35c755a96b70c5aa5dfc8d7ec1fd885e42
kurtprime <kurtquejada@gmail.com>
2023-01-22 15:13:12 +0800

    clinic

---
+++ Lesson 2.0/clinic.js
@@ -0,0 +1,8 @@
+
+function clinic (lastname, firstname){
+    const fullname = [firstname + " "+lastname]
+   // fullname.join(' ')
+    console.log(`Hello, ${fullname}! How can we help you today?`)
+}
+
+clinic("Quejada", "Kurt Ivan");
