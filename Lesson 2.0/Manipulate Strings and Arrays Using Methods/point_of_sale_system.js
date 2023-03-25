97d4414ee15163ece75cf038a1a6e27603d29830
kurtprime <kurtquejada@gmail.com>
2023-03-25 12:14:08 +0800

    new file location

---
+++ Lesson 2.0/Manipulate Strings and Arrays Using Methods/point_of_sale_system.js
@@ -0,0 +1,22 @@
+const store_inventory = {
+    Obj:[
+        {
+            item: "Shampoo",
+            quantity: 1,
+            price: 3
+        },
+        {
+            item: "soap",
+            quantity: 0,
+            price: 2
+        },
+        {
+            item: "toothpaste",
+            quantity: 2,
+            price: 3
+        },
+    ] 
+    
+}
+
+console.table(store_inventory)
