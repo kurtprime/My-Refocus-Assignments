cfd6cd950f947d8a1200e85c1ad7f5af7e377b78
kurtprime <kurtquejada@gmail.com>
2023-06-22 22:59:48 +0800

    commit

---
+++ Module 3 react/react-router/src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div className=" bg-background bg-cover bg-fixed  bg-no-repeat h-screen w-full  overflow-x-hidden overflow-y-hidden absolute  ">
+      <div className=" h-screen w-screen opacity-50 bg-black ">
+        <div className="h-full w-full flex justify-center items-center">
+          <h2 className="text-white ">Hello everyone</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
